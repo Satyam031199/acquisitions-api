dc6aca7b2d3661b4d55416853e5c723be4e1b90c
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2025-09-15 13:20:36 +0530

    add testing

--- src/app.js
+++ src/app.js
@@ -42,4 +42,11 @@ app.get('/api', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 
+app.use((req,res) => {
+    res.status(404).json({
+        error: 'Not Found',
+        message: 'The requested resource does not exist'
+    })
+})
+
 export default app;
